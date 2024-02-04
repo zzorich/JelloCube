e5361479182aaf0c294851a59f3dc770d308a84c
lingji zhou <lingji.zhou17@gmail.com>
2024-02-04 15:00:06 -0800

    Initial Commit

---
+++ ElasticJello/ElasticJelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElasticJelloApp.swift
+//  ElasticJello
+//
+//  Created by lingji zhou on 2/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ElasticJelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
